1bd7c7d7b9f67c3978a62c0ab7fefc605cecc55b
Seapolco <seapolco@gmail.com>
2022-09-07 20:57:26 +0100

    Making steps towards accepting users input for the grid dimension.
    	- added prompt
    	-tested targeting the css from javascript
    
    	To-Do
    	-create a function to pass number of squares, calculated from
    	 users input, into the for loop.
    	-function to change the CSS for the container.

--- app.js
+++ app.js
@@ -1,13 +1,20 @@
 
 
 let header = document.querySelector('.header');
-let container = document.querySelector('#container');
+let container = document.querySelector('.container');
 
+let usersGrid = parseInt(prompt('How many columns/grid ye like?'));
+
+console.log(usersGrid)
+
+let numOfSquares = usersGrid * usersGrid;
+
+console.log(numOfSquares)
 
 
 
 // Populate the container with a 16x16 grid of squares (divs);
-for(let i = 0; i < 256; i++) {
+for(let i = 0; i < 1156; i++) {
     let square = document.createElement('div');
     // let divContent = document.createTextNode('');
     square.classList.add('square');
@@ -29,6 +36,9 @@ squareDiv.forEach(square => {
     })
     
 })
+
+//container.style.gridTemplateColumns = "repeat(16,1fr)";
+
 
 // squareDiv.forEach(square => {
 //     square.addEventListener('mouseleave', (e) => {
