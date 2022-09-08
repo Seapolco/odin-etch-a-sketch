559460e8d3cdf5a7bad451ff0aa3f61b1114c60a
Seapolco <seapolco@gmail.com>
2022-09-08 16:01:02 +0100

    Successfully implemented user input.
    I was banging my head against a wall for a quite some time before
    realising that the issue a simple one, I variable used in the
    'generateGrid()' function was different to the one used in the for loop
    (numberOfSquares & numOfSquares).
    
    Nice! Feeling good.
    
    I've also added a function to generate randomRBG codes for the rainbow
    effect. Just need to link this up to the button and do some final
    polishing and then the app will be complete.
    
     # Please enter the commit message for
    your changes. Lines starting

--- app.js
+++ app.js
@@ -3,12 +3,45 @@
 let header = document.querySelector('.header');
 let container = document.querySelector('.container');
 let button = document.querySelector('.userInput');
+let body = document.querySelector('body');
+
+let usersGrid = 32;
+let numOfSquares = usersGrid * usersGrid;
+
+
+console.log(body)
 
 // let usersGrid = parseInt(prompt('Number of columns/rows?'));
 
 
-let usersGrid = 16;
-let numOfSquares = usersGrid * usersGrid;
+
+function deleteGrid() {
+  return  usersGrid = 0;
+}
+
+function randomColor() {
+	let random1 = Math.floor(Math.random() * 255) + 1
+  	let random2 = Math.floor(Math.random() * 255) + 1
+    let random3 = Math.floor(Math.random() * 255) + 1
+    let red,green,blue;
+  
+  return `rgb(${random1},${random2},${random3})`
+  
+  
+}
+
+function addHover() {
+
+    let squareDiv = document.querySelectorAll('.square');
+
+    squareDiv.forEach(square => {
+    square.addEventListener('mouseover', (e) => {
+      // console.log(e)
+      e.target.style.backgroundColor = `green`;
+    //   e.target.style.backgroundColor = `${randomColor()}`;
+   })
+  })
+}
 
 
 
@@ -48,51 +81,73 @@ let numOfSquares = usersGrid * usersGrid;
 
 // changeHeader(header);
 
+
 function generateGrid() {
 
-  
+    container.replaceChildren();
     
 
     for(let i = 0; i < numOfSquares; i++) {
+            
             let square = document.createElement('div');
             // let divContent = document.createTextNode('');
             square.classList.add('square');
             // div.appendChild(divContent);
             container.insertAdjacentElement('beforeend', square); 
+            // container.classList.add('container');
+            
+            console.log(`forloop${numOfSquares}`)
         }
-        
+    
+    
+    // container.parentNode.replaceChild(newContainer, container)
+    addHover();
 }
 
+31
 
 
 generateGrid();
 
 
-let squareDiv = document.querySelectorAll('.square');
 
-squareDiv.forEach(square => {
-    square.addEventListener('mouseover', (e) => {
-      // console.log(e)
-      e.target.style.backgroundColor = 'green';
-   })
-   
-})
 
+container.style.display = 'grid';
+container.style.justifyContent = 'center';
+container.style.gridTemplateColumns = `repeat(${usersGrid},1fr)`;
+container.style.gridTemplateRows = `repeat(${usersGrid},1fr)`;
+container.style.width = '500px';
+container.style.height = '500px';
+
+// newContainer.style.display = 'grid';
+// newContainer.style.justifyContent = 'center';
+// newContainer.style.gridTemplateColumns = `repeat(${usersGrid},1fr)`;
+// newContainer.style.width = '600px';
+// newContainer.style.height = '600px';
 
 
 
 button.addEventListener('click', () => {
     usersGrid = parseInt(prompt('How many squares?'));
-    numberOfSquares = usersGrid * usersGrid;
+    if(usersGrid > 100) {
+        usersGrid = 100;
+    }
+    numOfSquares = usersGrid * usersGrid;
+    console.log(`usersGrid${usersGrid}`)
     container.style.gridTemplateColumns = `repeat(${usersGrid},1fr)`;
+    container.style.gridTemplateRows = `repeat(${usersGrid},1fr)`;
+    container.style.display = 'grid';
+    container.style.justifyContent = 'center';
+    container.style.width = '500px';
+    container.style.height = '500px';
     generateGrid();
+    
 });
 
-container.style.display = 'grid';
-container.style.justifyContent = 'center';
-container.style.gridTemplateColumns = `repeat(${usersGrid},1fr)`;
-container.style.width = '600px';
-container.style.height = '600px';
+
+
+
+
 
 
 // button.addEventListener('click', () => {
