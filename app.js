8854dcc654610db8f965b35726ce25a2fc7a55c7
Seapolco <seapolco@gmail.com>
2022-09-08 13:59:05 +0100

    BUGS! - Added function which update users input after button/prompt.
    Feels like I am making progress but the current build is buggy. The input
    add's the squares to the last grid. Will now try and figure out a function
    which clear the grid before adding the new grid with the updated
    dimensions.

--- app.js
+++ app.js
@@ -2,42 +2,112 @@
 
 let header = document.querySelector('.header');
 let container = document.querySelector('.container');
+let button = document.querySelector('.userInput');
 
-let usersGrid = parseInt(prompt('How many columns/grid ye like?'));
+// let usersGrid = parseInt(prompt('Number of columns/rows?'));
 
-console.log(usersGrid)
 
+let usersGrid = 16;
 let numOfSquares = usersGrid * usersGrid;
 
-console.log(numOfSquares)
+
+
+// console.log(usersGrid)
+
+
+
+// console.log(numOfSquares)
+
+
 
 
 
 // Populate the container with a 16x16 grid of squares (divs);
-for(let i = 0; i < 1156; i++) {
-    let square = document.createElement('div');
-    // let divContent = document.createTextNode('');
-    square.classList.add('square');
-    // div.appendChild(divContent);
-    container.insertAdjacentElement('beforeend', square); 
-}
+// for(let i = 0; i < numOfSquares; i++) {
+//     let square = document.createElement('div');
+//     // let divContent = document.createTextNode('');
+//     square.classList.add('square');
+//     // div.appendChild(divContent);
+//     container.insertAdjacentElement('beforeend', square); 
+// }
 
 
 // Create "hover" event listener
 
-let squareDiv = document.querySelectorAll('.square');
+
 
 // console.log(squareDiv)
 
-squareDiv.forEach(square => {
-     square.addEventListener('mouseover', (e) => {
-       // console.log(e)
-       e.target.style.backgroundColor = 'green';
-    })
+
+
+            
+
+    // function changeHeader(...header) {
+    //     return header[0].textContent = 'Loooool';
+    // } 
+
+// changeHeader(header);
+
+function generateGrid() {
+
+  
     
+
+    for(let i = 0; i < numOfSquares; i++) {
+            let square = document.createElement('div');
+            // let divContent = document.createTextNode('');
+            square.classList.add('square');
+            // div.appendChild(divContent);
+            container.insertAdjacentElement('beforeend', square); 
+        }
+        
+}
+
+
+
+generateGrid();
+
+
+let squareDiv = document.querySelectorAll('.square');
+
+squareDiv.forEach(square => {
+    square.addEventListener('mouseover', (e) => {
+      // console.log(e)
+      e.target.style.backgroundColor = 'green';
+   })
+   
 })
 
-//container.style.gridTemplateColumns = "repeat(16,1fr)";
+
+
+
+button.addEventListener('click', () => {
+    usersGrid = parseInt(prompt('How many squares?'));
+    numberOfSquares = usersGrid * usersGrid;
+    container.style.gridTemplateColumns = `repeat(${usersGrid},1fr)`;
+    generateGrid();
+});
+
+container.style.display = 'grid';
+container.style.justifyContent = 'center';
+container.style.gridTemplateColumns = `repeat(${usersGrid},1fr)`;
+container.style.width = '600px';
+container.style.height = '600px';
+
+
+// button.addEventListener('click', () => {
+//     let number = prompt('hewrooo??!');
+//     usersGrid = number;
+// })
+
+
+
+
+// display: grid;
+// justify-content: center;
+// grid-template-columns: repeat(16,1fr);
+// width: 600px;
+// height: 600px;
 
 
 // squareDiv.forEach(square => {
