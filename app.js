14245f045469511935c240c3955ebf0f65a602ef
Seapolco <seapolco@gmail.com>
2022-09-08 20:15:29 +0100

    Added the random color button + green,yellow,blue,orange.
    Required functionality complete!
    Zero work has been done on the UI/UX, so its time to zhuzh it up!

--- app.js
+++ app.js
@@ -4,20 +4,19 @@ let header = document.querySelector('.header');
 let container = document.querySelector('.container');
 let button = document.querySelector('.userInput');
 let body = document.querySelector('body');
+let randomMode = document.querySelector('.random');
+let greenMode = document.querySelector('.green');
+let yellowMode = document.querySelector('.yellow');
+let blueMode = document.querySelector('.blue');
+let orangeMode = document.querySelector('.orange');
+
 
 let usersGrid = 32;
 let numOfSquares = usersGrid * usersGrid;
 
-
-console.log(body)
-
-// let usersGrid = parseInt(prompt('Number of columns/rows?'));
+let colorMode = 'green';
 
 
-
-function deleteGrid() {
-  return  usersGrid = 0;
-}
 
 function randomColor() {
 	let random1 = Math.floor(Math.random() * 255) + 1
@@ -29,28 +28,134 @@ function randomColor() {
   
   
 }
+// let currentColor = 
 
-function addHover() {
+function addHover(color) {
 
     let squareDiv = document.querySelectorAll('.square');
-
+    console.log(`ADD${squareDiv}`)
     squareDiv.forEach(square => {
     square.addEventListener('mouseover', (e) => {
-      // console.log(e)
-      e.target.style.backgroundColor = `green`;
-    //   e.target.style.backgroundColor = `${randomColor()}`;
+      console.log(color);
+      if(color === 'rainbow') {
+        e.target.style.backgroundColor = `${randomColor()}` 
+      } else if(color === 'green') {
+        e.target.style.backgroundColor = 'green';
+      } else if(color === 'yellow') {
+        e.target.style.backgroundColor = 'yellow';
+      } else if(color === 'blue') {
+        e.target.style.backgroundColor = 'blue';
+      } else if(color === 'orange') {
+        e.target.style.backgroundColor = 'orange';
+      }
+        
+
+      
+
+    //   e.target.style.backgroundColor = ;
    })
   })
 }
 
+randomMode.addEventListener('click', () => {
+//   let squareDiv = document.querySelectorAll('.square');
+//   console.log(`RAND${squareDiv}`)
+ 
+
+//   squareDiv.forEach(square => {
+//   square.addEventListener('mouseover', (e) => {
+//     // console.log(e)
+//     // e.target.style.backgroundColor = squareColor;
+//     e.target.style.backgroundColor = `${randomColor()}` ;
+//  })
+// })
+  colorMode = 'rainbow';
+  generateGrid('rainbow');
+})
+
+greenMode.addEventListener('click', () => {
+  colorMode = 'green';
+  generateGrid('green');
+});
+yellowMode.addEventListener('click', () => {
+  colorMode = 'yellow';
+  generateGrid('yellow');
+})
+blueMode.addEventListener('click', () => {
+  colorMode = 'blue';
+  generateGrid('blue');
+})
+orangeMode.addEventListener('click', () => {
+  colorMode = 'orange';
+  generateGrid('orange');
+})
 
 
-// console.log(usersGrid)
+function generateGrid(color) {
+
+    container.replaceChildren();
+    
+
+    for(let i = 0; i < numOfSquares; i++) {
+            
+            let square = document.createElement('div');
+            // let divContent = document.createTextNode('');
+            square.classList.add('square');
+            // div.appendChild(divContent);
+            container.insertAdjacentElement('beforeend', square); 
+            // container.classList.add('container');
+            
+            console.log(`forloop${numOfSquares}`)
+        }
+    
+    
+    // container.parentNode.replaceChild(newContainer, container)
+    addHover(color);
+}
+
+31
+
+
+generateGrid('green');
 
 
 
-// console.log(numOfSquares)
 
+container.style.display = 'grid';
+container.style.justifyContent = 'center';
+container.style.gridTemplateColumns = `repeat(${usersGrid},1fr)`;
+container.style.gridTemplateRows = `repeat(${usersGrid},1fr)`;
+container.style.width = '500px';
+container.style.height = '500px';
+
+button.addEventListener('click', () => {
+    usersGrid = parseInt(prompt('How many squares?'));
+    if(usersGrid > 100) {
+        usersGrid = 100;
+    }
+    numOfSquares = usersGrid * usersGrid;
+    console.log(`usersGrid${usersGrid}`)
+    container.style.gridTemplateColumns = `repeat(${usersGrid},1fr)`;
+    container.style.gridTemplateRows = `repeat(${usersGrid},1fr)`;
+    container.style.display = 'grid';
+    container.style.justifyContent = 'center';
+    container.style.width = '500px';
+    container.style.height = '500px';
+    generateGrid(colorMode);
+    
+});
+
+// let square = document.querySelector('.square');
+// console.log(square.style.backgroundColor)
+
+// let current
+
+
+// newContainer.style.display = 'grid';
+// newContainer.style.justifyContent = 'center';
+// newContainer.style.gridTemplateColumns = `repeat(${usersGrid},1fr)`;
+// newContainer.style.width = '600px';
+// newContainer.style.height = '600px';
 
 
 
@@ -80,72 +185,6 @@ function addHover() {
     // } 
 
 // changeHeader(header);
-
-
-function generateGrid() {
-
-    container.replaceChildren();
-    
-
-    for(let i = 0; i < numOfSquares; i++) {
-            
-            let square = document.createElement('div');
-            // let divContent = document.createTextNode('');
-            square.classList.add('square');
-            // div.appendChild(divContent);
-            container.insertAdjacentElement('beforeend', square); 
-            // container.classList.add('container');
-            
-            console.log(`forloop${numOfSquares}`)
-        }
-    
-    
-    // container.parentNode.replaceChild(newContainer, container)
-    addHover();
-}
-
-31
-
-
-generateGrid();
-
-
-
-
-container.style.display = 'grid';
-container.style.justifyContent = 'center';
-container.style.gridTemplateColumns = `repeat(${usersGrid},1fr)`;
-container.style.gridTemplateRows = `repeat(${usersGrid},1fr)`;
-container.style.width = '500px';
-container.style.height = '500px';
-
-// newContainer.style.display = 'grid';
-// newContainer.style.justifyContent = 'center';
-// newContainer.style.gridTemplateColumns = `repeat(${usersGrid},1fr)`;
-// newContainer.style.width = '600px';
-// newContainer.style.height = '600px';
-
-
-
-button.addEventListener('click', () => {
-    usersGrid = parseInt(prompt('How many squares?'));
-    if(usersGrid > 100) {
-        usersGrid = 100;
-    }
-    numOfSquares = usersGrid * usersGrid;
-    console.log(`usersGrid${usersGrid}`)
-    container.style.gridTemplateColumns = `repeat(${usersGrid},1fr)`;
-    container.style.gridTemplateRows = `repeat(${usersGrid},1fr)`;
-    container.style.display = 'grid';
-    container.style.justifyContent = 'center';
-    container.style.width = '500px';
-    container.style.height = '500px';
-    generateGrid();
-    
-});
-
-
-
 
 
 
